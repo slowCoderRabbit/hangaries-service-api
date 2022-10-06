0051804e0683c56f3b31bf16893a7cece51ca6f5
slowcoderrabbit <slowcoderabbit@gmail.com>
2022-10-06 23:10:38 +0800

    PayU success and failure URL

--- src/main/java/com/hangaries/controller/IndexController.java
+++ src/main/java/com/hangaries/controller/IndexController.java
@@ -1,17 +1,82 @@
 package com.hangaries.controller;
 
 
+import com.hangaries.model.payu.PayUResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class IndexController {
+    static final String url = "https://client-app-uumgqhekpa-el.a.run.app/new-checkout?";
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+    private static final String queryParam = "page=success&";
+    static String htmlPart1Success = "<!DOCTYPE html>\n" +
+            "<html lang=\"en\">\n" +
+            "  <head>\n" +
+            "    <meta charset=\"UTF-8\" />\n" +
+            "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n" +
+            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+            "    <title>Payment Successful!!</title>\n" +
+            "  </head>\n" +
+            "  <body style=\"text-align: center\">\n" +
+            "    <script>\n" +
+            "      function stateChange() {\n" +
+            "        setTimeout(function () {\n" +
+            "          window.location.replace(\"";
 
+    static String htmlPart2Success = "\");\n" +
+            "        }, 3000);\n" +
+            "      }\n" +
+            "    </script>\n" +
+            "    <div>\n" +
+            "      <h1>Sample Invoice</h1>\n" +
+            "      <h3>Success Page</h3>\n" +
+            "      <h3>\n" +
+            "        Browser will stay 3 seconds in this page and automatically redirect you\n" +
+            "        to the Client APP\n" +
+            "      </h3>\n" +
+            "    </div>\n" +
+            "    <script>\n" +
+            "      stateChange();\n" +
+            "    </script>\n" +
+            "  </body>\n" +
+            "</html>";
 
-    public static final String STRING = "<!DOCTYPE html>\n" +
+    static String htmlPart1Failure = "<!DOCTYPE html>\n" +
+            "<html lang=\"en\">\n" +
+            "  <head>\n" +
+            "    <meta charset=\"UTF-8\" />\n" +
+            "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n" +
+            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+            "    <title>Payment Failed!!</title>\n" +
+            "  </head>\n" +
+            "  <body style=\"text-align: center\">\n" +
+            "    <script>\n" +
+            "      function stateChange() {\n" +
+            "        setTimeout(function () {\n" +
+            "          window.location.replace(\"";
+
+    static String htmlPart2Failure = "\");\n" +
+            "        }, 3000);\n" +
+            "      }\n" +
+            "    </script>\n" +
+            "    <div>\n" +
+            "      <h1>Sample Invoice</h1>\n" +
+            "      <h3>Failure Page</h3>\n" +
+            "      <h3>\n" +
+            "        Browser will stay 3 seconds in this page and automatically redirect you\n" +
+            "        to the Client APP\n" +
+            "      </h3>\n" +
+            "    </div>\n" +
+            "    <script>\n" +
+            "      stateChange();\n" +
+            "    </script>\n" +
+            "  </body>\n" +
+            "</html>";
+    String a = "<!DOCTYPE html>\n" +
             "<html lang=\"en\">\n" +
             "  <head>\n" +
             "    <meta charset=\"UTF-8\" />\n" +
@@ -41,16 +106,69 @@ public class IndexController {
             "      stateChange();\n" +
             "    </script>\n" +
             "  </body>\n" +
-            "</html>";
+            "</html>\n";
 
-    @PostMapping("savePayUResponse")
-    public String sayHello2(@ModelAttribute String string) {
-        return STRING;
+    private static String generateQueryParam(PayUResponse response) {
+        StringBuilder qp = new StringBuilder(queryParam);
+        qp.append("status=");
+        qp.append(response.getStatus());
+        qp.append("&");
+        qp.append("txnid=");
+        qp.append(response.getTxnid());
+        qp.append("&");
+        qp.append("hash=");
+        qp.append(response.getHash());
+        qp.append("&");
+        qp.append("error_Message=");
+        qp.append(response.getError_Message());
+        return qp.toString();
     }
+
+    @PostMapping("savePayUResponseSuccess")
+    public String savePayUResponseSuccess(@ModelAttribute PayUResponse response) {
+        logger.info("PayU transaction successful :: [{}]", response);
+        String result = generateQueryParam(response);
+        String responseHTML = htmlPart1Success + url + result + htmlPart2Success;
+        logger.info("Success response HTML = [{}]", responseHTML);
+        return responseHTML;
+    }
+
+    @PostMapping("savePayUResponseFailure")
+    public String savePayUResponseFailure(@ModelAttribute PayUResponse response) {
+        logger.info("PayU transaction failed :: [{}]", response);
+        String result = generateQueryParam(response);
+        String responseHTML = htmlPart1Failure + url + result + htmlPart2Failure;
+        logger.info("Failure response HTML = [{}]", responseHTML);
+        return responseHTML;
+    }
+
+//    public static void main(String[] args) {
+//
+////        PayUResponse payUResponse = new PayUResponse();
+////        payUResponse.setStatus("Success");
+////        payUResponse.setHash("Hash");
+////        payUResponse.setTxnid("asdfasdfasd");
+////        payUResponse.setError_Message("no error");
+////        String result = generateQueryParam(payUResponse);
+////        System.out.println(result);
+////
+////        System.out.println(result);
+////        System.out.println(htmlPart1+url+result+htmlPart2);
+//
+//
+//http://192.168.10.122:8080/api/savePayUResponseSuccess
+//
+//http://192.168.10.122:8080/api/savePayUResponseFailure
+
+//    https://api-playground.payu.in/#
+//   https://developers.payu.com/en/overview.html#sandbox_cards
+//
+//
+//    }
 
     @GetMapping("savePayUResponse")
     public String sayHello() {
-        return STRING;
+        return "STRING";
     }
 
 }
---
+++ src/main/java/com/hangaries/model/payu/PayUResponse.java
@@ -0,0 +1,15 @@
+package com.hangaries.model.payu;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayUResponse {
+    private String status;
+    private String txnid;
+    private String error_Message;
+    private String hash;
+}
